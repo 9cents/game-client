d5ea391b04fa93afee7165eb1cf6e4e8e0fd2023
Vincentia <62258168+kVincentia@users.noreply.github.com>
2021-03-10 22:29:11 +0800

    Add files via upload

---
+++ splashscreen/script/splashscreen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _input(event):
+	if(event is InputEventKey):
+		go_title_screen()
+
+func go_title_screen():
+	get_tree().change_scene("res://splashscreen.tscn")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	go_title_screen()
+	pass # Replace with function body.
