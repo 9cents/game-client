322542433ce71230db9d66c330264baf160a9766
Chengyunl <73602909+Chengyunl@users.noreply.github.com>
2021-03-18 04:38:02 +0800

    UpdateHelpScreen

--- World_Selection/Interface/Help.gd
+++ World_Selection/Interface/Help.gd
@@ -6,9 +6,9 @@ extends CanvasLayer
 # var b = "text"
 
 onready var MainButtonScreen = get_node("ColorRect/MainButton/Main_Help")
+onready var StoryButtonScreen = get_node("ColorRect/StoryButton/Story_Help")
 onready var ChallengeButtonScreen = get_node("ColorRect/ChallengeButton/Challenge_Help")
 onready var GameplayButtonScreen = get_node("ColorRect/GameplayButton/Gameplay_Help")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -20,23 +20,33 @@ func _ready():
 
 func _on_MainButton_pressed():
 	MainButtonScreen.show()
+	StoryButtonScreen.hide()
 	ChallengeButtonScreen.hide()
 	GameplayButtonScreen.hide()
 	pass # Replace with function body.
 
-
+func _on_StoryButton_pressed():
+	MainButtonScreen.hide()
+	StoryButtonScreen.show()
+	ChallengeButtonScreen.hide()
+	GameplayButtonScreen.hide()
+	pass # Replace with function body.
+	
 func _on_ChallengeButton_pressed():
 	MainButtonScreen.hide()
+	StoryButtonScreen.hide()
 	ChallengeButtonScreen.show()
 	GameplayButtonScreen.hide()
 	pass # Replace with function body.
-
+	
 func _on_GameplayButton_pressed():
 	MainButtonScreen.hide()
+	StoryButtonScreen.hide()
 	ChallengeButtonScreen.hide()
 	GameplayButtonScreen.show()
 	pass # Replace with function body.
-
+	
+	
 func _on_CloseButton_pressed():
 	ScreenSwitcher.change_scene("res://World_Selection/Map/MainPage.tscn")
 	pass # Replace with function body.
